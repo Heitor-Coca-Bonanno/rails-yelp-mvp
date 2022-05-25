ce3bcc864cd6f7b8179a3f4ebb924c4c69d20844
Heitor Bonanno <heitor.cb10@gmail.com>
2022-05-25 15:17:13 -0300

    Restaurant controller compleeted

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/create'
+  get 'restaurants/new'
+  get 'restaurants/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[new create]
+  end
 end
